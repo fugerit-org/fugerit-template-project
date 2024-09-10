357774cf15bf19367389e17aa7fcc1c61381049a
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-09-10 16:28:30 +0200

    Module fugerit-springboot-template, based on springboot 3.3.3 + SpringBoot Web and GraalVM support

---
+++ fugerit-springboot-template/src/main/java/org/fugerit/java/template/fugerit_springboot_template/FugeritSpringbootTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.fugerit.java.template.fugerit_springboot_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FugeritSpringbootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FugeritSpringbootTemplateApplication.class, args);
+	}
+
+}
---
+++ fugerit-springboot-template/src/test/java/org/fugerit/java/template/fugerit_springboot_template/FugeritSpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fugerit.java.template.fugerit_springboot_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FugeritSpringbootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
